08a08e78d53752514e772c9580912e7feb7eaa57
6303217506 <vanamdhanushnetha@gmail.com>
2024-12-27 11:28:05 +0530

    first commit

---
+++ src/context/DiagramContext.js
@@ -0,0 +1,14 @@
+
+import React, { createContext, useState } from 'react';
+
+export const DiagramContext = createContext();
+
+export const DiagramProvider = ({ children }) => {
+  const [diagramData, setDiagramData] = useState({ nodes: [], edges: [] });
+
+  return (
+    <DiagramContext.Provider value={{ diagramData, setDiagramData }}>
+      {children}
+    </DiagramContext.Provider>
+  );
+};
